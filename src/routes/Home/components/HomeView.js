5ac6d23fac8faa64cdd73df8e6ba00f427580958
Alan Yu <alanyu2015@gmail.com>
2017-03-26 17:53:21 +0800

    Added google-map-react

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import GoogleMapReact from 'google-map-react'
 import './HomeView.scss'
 import Search from 'containers/SearchContainer'
 
 export const HomeView = () => (
   <div>
     <Search />
+    <div className='map'>
+      <GoogleMapReact
+        defaultCenter={{ lat: 59.95, lng: 30.33 }}
+        defaultZoom={11}
+      />
+    </div>
   </div>
 )
 
